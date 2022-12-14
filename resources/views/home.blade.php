a0d9a3322552c3b9043ad23dd1c6ee5326b06d4b
anandhtl <anddhital@gmail.com>
2022-12-14 22:06:34 +0545

    14 December

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,43 +33,44 @@
             </div>
         </div>
     </section>
-    <section id="navbar">
-        <nav class="navbar navbar-expand-lg ">
-            <a style="color:black" class="navbar-brand" href="#">My Own Site</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Blog</a>
-                    </li>
+    <section id="navbar" class="sticky">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="#">myOwnSite</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
 
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline- my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
-        </nav>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav text-center ml-auto">
+                        <li class="nav-item ">
+                            <a class="nav-link" href="">Home</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="">About</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="">Service</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                                Our Classes
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#" style="border-bottom: 2px solid var(--s-color);">Web Development</a>
+                                <a class="dropdown-item" href="#" style="border-bottom: 2px solid var(--s-color);">Mobile Develpment</a>
+                                <a class="dropdown-item" href="#">Seo Marketing</a>
+                            </div>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </section>
     </section>
     <!-- Navbar ends here -->
     <!-- slider section starts here -->
@@ -212,6 +213,55 @@
         </div>
     </section>
     <!-- about us section ends  here -->
+    <!---our classes section starts here--->
+    <section id="ourclasses">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title">
+                        <h1>Our Classes </h1>
+                        <h2>Who we are? </h2>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="our-class-body">
+                        <i id="icon" class="fa-solid fa-laptop"></i>
+                        <h4>Web Development</h4>
+                        <ul>
+                            <li>Daily Classes Starts from 8:45 PM</li>
+                            <li>Daily Classes Starts from 8:45 PM</li>
+                            <li>Daily Classes Starts from 8:45 PM</li>
+                            <li>Daily Classes Starts from 8:45 PM</li>
+
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="our-class-body">
+                        <i id="icon" class="fa-solid fa-paperclip"></i>
+                        <h4>SEO Optimization</h4>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, mollitia harum. Dolores est nisi nam hic rerum. Laboriosam, nemo. Nemo ratione necessitatibus laborum aut distinctio libero qui. Sapiente, eveniet odio!</p>
+
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="our-class-body">
+                        <i id="icon" class="fa-solid fa-gear"></i>
+                        <h4>Flutter Development</h4>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo, mollitia harum. Dolores est nisi nam hic rerum. Laboriosam, nemo. Nemo ratione necessitatibus laborum aut distinctio libero qui. Sapiente, eveniet odio!</p>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+    <!---our classes section starts here--->
 
 </body>
 
